a1f08ad6e522adcd026ccdac547690ac9a19b239
ccmxy <windrainicefire@gmail.com>
2016-11-02 16:17:03 -0700

    Finished all tests

--- submissions/hw5/src/test/java/edu/osu/blackjack/SimpleBlackjackTest.java
+++ submissions/hw5/src/test/java/edu/osu/blackjack/SimpleBlackjackTest.java
@@ -14,11 +14,9 @@ public class SimpleBlackjackTest {
 
   @Test
   public void testMock(){
-// Does it run without errors for a simple case?
 DealerAction dt = mock(DealerAction.class);
 PlayerAction pa = mock(PlayerAction.class);
 
-//  This player is feeling charitiable....
 when(pa.getAction()).thenReturn(PlayerAction.ActionType.HIT)
                           .thenReturn(PlayerAction.ActionType.HIT)
                           .thenReturn(PlayerAction.ActionType.HIT)
@@ -29,7 +27,7 @@ when(pa.getAction()).thenReturn(PlayerAction.ActionType.HIT)
                     .thenReturn(PlayerAction.ActionType.STAND);
 SimpleBlackjack j = new SimpleBlackjack(dt,new PlayerAction[]{pa});
 j.playRound();
-//Sign up a bug: Deal card was not called 9 times.
+//Sign of a bug: Deal card was not called 9 times...
 verify(dt, times(9)).dealCard(pa);
   }
 
@@ -66,31 +64,46 @@ verify(dt, times(9)).dealCard(pa);
 
 
   @Test
-  public void dealerTipsEveryPlayer(){
+  public void dealerOffersBetToAllPlayers(){
 
     DealerAction da = mock(DealerAction.class);
     PlayerAction pa = mock(PlayerAction.class);
     PlayerAction pa2 = mock(PlayerAction.class);
     PlayerAction pa3 = mock(PlayerAction.class);
-    CommonAction ca = mock(CommonAction.class);
-
 
     when(pa.getAction()).thenReturn(PlayerAction.ActionType.STAND);
     when(pa2.getAction()).thenReturn(PlayerAction.ActionType.STAND);
     when(pa3.getAction()).thenReturn(PlayerAction.ActionType.STAND);
 
-  //  when(da.dealCard(ca)).thenReturn()
-
-    // InOrder inOrder = inOrder(pa,pa2,deal);  // have tried moving this above and below blackjack instantiation
-
     SimpleBlackjack j = new SimpleBlackjack(da,new PlayerAction[]{pa,pa2,pa3});
     j.playRound();
 
-    InOrder inOrder = inOrder(pa,pa2,da);  //  **** ERROR ON THIS LINE ***
-    // inOrder.verify(pa, times(1)).makeBet();
+    verify(pa, times(1)).makeBet();
+    verify(pa2, times(1)).makeBet();
+    verify(pa3, times(1)).makeBet();
 
-    // InOrder.verify(pa2, times(1)).makeBet();\\acceptCard
   }
 
 
+
+/*Test to ensure that doubling down forces players to
+double wager, take a single card and finish*/
+  @Test
+  public void testDoubleDownFunctionality() {
+    DealerAction da = mock(DealerAction.class);
+    PlayerAction pa = mock(PlayerAction.class);
+
+    when(pa.getAction()).thenReturn(PlayerAction.ActionType.DOUBLE);
+
+    SimpleBlackjack j = new SimpleBlackjack(da,new PlayerAction[]{pa});
+    j.playRound();
+
+    //create inOrder object passing any mocks that need to be verified in order
+    InOrder inOrder = inOrder(pa, da);
+
+    //ensure that they were only allowed to take one action
+    inOrder.verify(pa, times(1)).getAction();
+    //ensure that that action was to double down on their bet
+    inOrder.verify(pa, times(1)).doubleDownBet();
+  }
 }
